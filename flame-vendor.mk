07765b3fdfc55456114f6b0fce17442b701bde2e
Michael Bestas <mkbestas@lineageos.org>
2024-09-08 02:47:41 +0300

    flame: Add xml suffix to prebuilt_etc_xml modules
    Change-Id: I67745a8a9bdb7c3a8a98509aa3efaaf7cab75a17

--- flame-vendor.mk
+++ flame-vendor.mk
@@ -121,8 +121,8 @@ PRODUCT_PACKAGES += \
     com.google.android.camera.extensions \
     google-ril \
     qcrilhook \
-    manifest_wifi_ext_aidl \
-    vendor.qti.hardware.radio.atcmdfwd@1.0
+    manifest_wifi_ext_aidl.xml \
+    vendor.qti.hardware.radio.atcmdfwd@1.0.xml
 
 PRODUCT_PACKAGES += \
     libdmengine_libdmengine_symlink32 \
@@ -1916,15 +1916,15 @@ PRODUCT_PACKAGES += \
     adreno_graphics_driver \
     gpu_profiling_vulkan_layer \
     com.google.android.camera.experimental2019 \
-    android.hardware.confirmationui@1.0-service-google \
-    android.hardware.identity.strongbox \
-    android.hardware.keymaster@4.1-service.citadel \
-    android.hardware.neuralnetworks@1.3-service-qti \
-    android.hardware.weaver@1.0-service.citadel \
-    manifest_android.hardware.drm-service.widevine \
-    manifest_input.processor-service \
-    rebootescrow-citadel \
-    vendor.google.wireless_charger@1.3-service-vendor
+    android.hardware.confirmationui@1.0-service-google.xml \
+    android.hardware.identity.strongbox.xml \
+    android.hardware.keymaster@4.1-service.citadel.xml \
+    android.hardware.neuralnetworks@1.3-service-qti.xml \
+    android.hardware.weaver@1.0-service.citadel.xml \
+    manifest_android.hardware.drm-service.widevine.xml \
+    manifest_input.processor-service.xml \
+    rebootescrow-citadel.xml \
+    vendor.google.wireless_charger@1.3-service-vendor.xml
 
 PRODUCT_PACKAGES += \
     libEGL_adreno_libEGL_adreno_symlink32 \
